3361adbffa11a34d0de15c0a59895bac34eaa368
Kevin Mugele <kevin.mugele@gmail.com>
2021-08-25 18:17:18 -0400

    Work through User group 12

--- app/controllers/team_players_controller.rb
+++ app/controllers/team_players_controller.rb
@@ -4,4 +4,20 @@ class TeamPlayersController < ApplicationController
     @team = Team.find(params[:team_id])
     @players = @team.players
   end
+
+  def new
+  end
+
+  def create
+    player = Player.new({
+      name: params[:player][:name],
+      number: params[:player][:number],
+      injured: params[:player][:injured],
+      team_id: params[:team_id]
+    })
+
+    player.save
+
+    redirect_to "/teams/:team_id/players"
+  end
 end
