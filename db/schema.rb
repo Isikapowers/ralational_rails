556da11706f647950ed4876fd27cdd0e8a34e96f
Kevin Mugele <kevin.mugele@gmail.com>
2021-08-25 08:38:30 -0400

    Commit schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_24_214517) do
+ActiveRecord::Schema.define(version: 2021_08_24_215748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.float "price"
     t.boolean "kids_friendly"
-  end 
+  end
 
   create_table "players", force: :cascade do |t|
     t.string "name"
@@ -28,8 +27,7 @@ ActiveRecord::Schema.define(version: 2021_08_24_214517) do
     t.boolean "injured"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end 
-
+  end
 
   create_table "stores", force: :cascade do |t|
     t.string "name"
